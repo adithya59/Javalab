5c719bdaa84e1db801b1e61be0e9de5a0e836e07
adithya59 <145443401+adithya59@users.noreply.github.com>
2023-10-11 02:46:51 -0700

    Update and rename MatAddition.java to MatrixAddition.java

--- MatrixAddition.java
+++ MatrixAddition.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class MatAddition {
+public class MatrixAddition {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int ROW1, ROW2, COL1, COL2;
