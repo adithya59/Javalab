a452dbf511cd626c8fbae9ab0b49b1b426a94d77
adithya59 <145443401+adithya59@users.noreply.github.com>
2023-11-04 10:45:02 +0530

    Update Interface.java

--- Interface.java
+++ Interface.java
@@ -2,7 +2,6 @@ public class Interface {
 	public static void main(String[] args) {
 		Fish d = new Fish("Mimi");
 		Cat c = new Cat("Fluffy");
-		Animal a = new Fish();
 		Animal e = new Spider();
 		Pet p = new Cat();
 		System.out.println("CAT");
